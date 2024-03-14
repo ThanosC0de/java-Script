d3f84580ff3aca531e73844f448f764286ed1c71
ThanosC0de <163315295+ThanosC0de@users.noreply.github.com>
2024-03-14 11:11:22 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,17 @@
+"use strict"; // treat all js code as newer version
+
+let name = "sahil"
+let age = 81
+let isLoggedIn = true
+
+/* number => 2 to power 53
+bigint
+string => ""
+boolone => true/false
+null => standalone value
+undefined =>
+symbol => unique
+
+object 
+*/
+console.log(typeof "sahil")
