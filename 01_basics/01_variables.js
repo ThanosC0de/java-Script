aa46b5aa9d78dede55bceb7b759a8e89ff436474
ThanosC0de <163315295+ThanosC0de@users.noreply.github.com>
2024-03-14 10:41:45 +0000

    conformation is done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,9 @@
+const accountId = 3650
+let accountName = "sahil"
+var accountGmail = "sahil@gmail.com"
+accountPassword = "3650"
+let accountState
+
+console.table([accountId, accountName, accountGmail, accountPassword, accountState])
+
+// VAR varible no use karvo nai because te block scope and functional scope ne issue kare che.
